0c6f651aae779d773df6d3bdab795f5fed590173
Jesus Daniel Hernandez <jhernandez4598@sdsu.edu>
2025-11-15 17:42:21 -0800

    added other pages

--- app/src/main/java/com/example/itemshare/MainActivity.kt
+++ app/src/main/java/com/example/itemshare/MainActivity.kt
@@ -86,18 +86,10 @@ enum class AppDestinations(
     SETTINGS("Settings", Icons.Default.Settings),
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ItemShareTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ItemShareTheme {
+//        Greeting("Android")
+//    }
+//}
