0c3fb19c139883203a246546d0a79ec8240a9a86
BryceHPersonal85 <bhemmie8667@sdsu.edu>
2025-11-16 10:59:20 -0800

    Replaced Settings with Messages and modified MainActivity to show this

---
+++ app/src/main/java/com/example/itemshare/Messages.kt
@@ -0,0 +1,30 @@
+package com.example.itemshare
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+
+@Composable
+fun Messages(modifier: Modifier = Modifier.fillMaxSize())
+{
+    Column(modifier)
+    {
+        Text("Messages will go here :)")
+    }
+}
